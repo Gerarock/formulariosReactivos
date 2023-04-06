1300bec196af39761de4d07a78d750d36689c634
gveliz90 <gveliz90@bancoestado.cl>
2023-04-06 01:37:10 -0400

    refactorizacion validacion apellido

--- src/app/components/formularios/formularios.component.ts
+++ src/app/components/formularios/formularios.component.ts
@@ -17,7 +17,7 @@ export class FormulariosComponent {
   ) {
     this.formClient = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30), Validators.pattern(/^[A-Za-z\s\xF1\xD1]+$/)]],
-      apellido: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      apellido: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30), Validators.pattern(/^[A-Za-z\s\xF1\xD1]+$/)]],
       email: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40), Validators.pattern(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/)]],
       telefono: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(10), Validators.pattern(/^([0-9])*$/)]],
       usuario: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
